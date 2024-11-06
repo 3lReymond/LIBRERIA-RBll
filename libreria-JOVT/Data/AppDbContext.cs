565fd5c4c17d95acee53adffd7799c93aad2c821
vegatona <oswaldovega756@gmail.com>
2024-11-06 14:52:05 -0700

    Agregar archivos de proyecto.

---
+++ libreria-JOVT/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using libreria_JOVT.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace libreria_JOVT.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
