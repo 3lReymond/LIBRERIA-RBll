ce7568dcb2ff2deac9f822851cd17a9dff862646
vegatona <oswaldovega756@gmail.com>
2024-11-07 13:19:10 -0700

    3.3

--- libreria-JOVT/Controllers/BooksController.cs
+++ libreria-JOVT/Controllers/BooksController.cs
@@ -15,10 +15,39 @@ namespace libreria_JOVT.Controllers
         {
             _booksService = booksService;
         }
+
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBks()
+        {
+            var allbooks = _booksService.GetAllBks();
+            return Ok(allbooks);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            return Ok(book);
+        }
+
         [HttpPost("add-book")]
         public IActionResult AddBook([FromBody]BookVM book)
         {
             _booksService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody]BookVM book)
+        {
+            var updateBook = _booksService.UpdateBookByID(id, book);
+            return Ok(updateBook);
+        }
+
+        [HttpDelete("delete-bool-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _booksService.DeleteBookById(id);
             return Ok();
         }
     }
--- libreria-JOVT/Data/Services/BooksService.cs
+++ libreria-JOVT/Data/Services/BooksService.cs
@@ -1,6 +1,9 @@
 ﻿using libreria_JOVT.Data.Models;
 using libreria_JOVT.Data.ViewModels;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace libreria_JOVT.Data.Models.Services
 {
@@ -27,6 +30,40 @@ namespace libreria_JOVT.Data.Models.Services
             };
             _context.Books.Add(_book);
             _context.SaveChanges();
+        }
+        //metodo que nos permite obtener la lista de todos los libros de la BD
+        public List<Book> GetAllBks() => _context.Books.ToList();
+        //Metodo que nos permite obtener el libro que estamos pidiendo de la BD
+        public Book GetBookById(int bookid) => _context.Books.FirstOrDefault(n => n.id == bookid);
+        
+        //METODO QUE NOS PERMITE MODIFICAR UN LIBRO QUE SE ENCUENTRE EN LA BD
+        public Book UpdateBookByID(int bookid, BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.id == bookid);
+            if(_book != null)
+            {
+                _book.Titulo = book.Titulo;
+                _book.Descripcion = book.Descripcion;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.DateRead;
+                _book.Rate = book.Rate;
+                _book.Genero = book.Genero;
+                _book.Autor = book.Autor;
+                _book.ConverUrl = book.ConverUrl;
+                _context.SaveChanges();
+            }
+            return _book;
+        }
+
+        //metodo para eliminar
+        public void DeleteBookById(int bookid)
+        {
+            var _book = _context.Books.FirstOrDefault(N => N.id == bookid);
+            if(_book != null)
+            {
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+            }
         }
     }
 }
