e67e232c15a3de4a2e94e2ba1f8acbae4c77475b
vegatona <oswaldovega756@gmail.com>
2024-11-12 14:35:05 -0700

    3.7

--- libreria-JOVT/Data/Services/BooksService.cs
+++ libreria-JOVT/Data/Services/BooksService.cs
@@ -47,7 +47,22 @@ namespace libreria_JOVT.Data.Models.Services
     //metodo que nos permite obtener la lista de todos los libros de la BD
     public List<Book> GetAllBks() => _context.Books.ToList();
         //Metodo que nos permite obtener el libro que estamos pidiendo de la BD
-        public Book GetBookById(int bookid) => _context.Books.FirstOrDefault(n => n.id == bookid);
+        public BookWithAuthorsVM GetBookById(int bookid)
+        {
+            var _bookWithAuthors = _context.Books.Where(n => n.id == bookid).Select(book => new BookWithAuthorsVM()
+            {
+                Titulo = book.Titulo,
+                Descripcion = book.Descripcion,
+                IsRead = book.IsRead,
+                DateRead = book.DateRead,
+                Rate = book.Rate,
+                Genero = book.Genero,
+                ConverUrl = book.ConverUrl,
+                PublisherName = book.Publisher.Name,
+                AutorNames = book.book_Authors.Select(n => n.Author.FullName).ToList()
+            }).FirstOrDefault();
+            return _bookWithAuthors;
+        }
         
         //METODO QUE NOS PERMITE MODIFICAR UN LIBRO QUE SE ENCUENTRE EN LA BD
         public Book UpdateBookByID(int bookid, BookVM book)
